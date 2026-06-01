86abc2c56cf9fbec7b4d0e5713843e9bd8e9fdef
peedro367 <pc621528@gmail.com>
2026-06-01 14:24:53 +0000

    366

--- calc_imc.py
+++ calc_imc.py
@@ -1,4 +1,8 @@
 
 #pedro henryke da costa gomes
-def calcular_imc(p, a):
-    return p / (a ** p)
+def calcular_imc(2, a):
+    return p / (a ** 2)
+
+def classificar(valor_imc):
+    if imc > 25:
+
