84901a0db170f6d1d3ece8eeb01e42e2d3c94b60
rafaelht05 <rafael.ht05@gmail.com>
2026-06-03 01:04:22 +0000

    v3

--- calc_imc.py
+++ calc_imc.py
@@ -1,4 +1,21 @@
 
-#pedro henryke da costa gomes
-def calcular_imc(2, a):
-    return p / (a ** 2)
+#Módulo 1: Lógica Matemática
+#Responsável: Pedro Henryke da Costa Gomes
+def calcular_imc(p, a):
+    imc = p / (a ** 2)
+    return imc
+
+#Módulo 2: Classificação de Dados
+#Módulo 3: Especialista em Conteúdo
+#Responsável: Rafael Henrique Trajano
+def classificar(valor_imc):
+    if valor_imc <25:
+        print("Saudável ✅")
+    else:
+        print("Acima do peso 🚫")
+        
+def gerar_aviso(status):
+    if imc <25:
+        print("Dica: Continue mantendo hábitos saudáveis, como uma dieta equilibrada e exercícios regulares!")
+    else:
+        print("Dica: Inclua caminhadas na sua rotina e foque em alimentos ricos em fibras. Pequenas mudanças fazem grande diferença!")
