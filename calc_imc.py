d872d191cb57ca828b1f195b8aab4c9f802058ff
nardiniryan5-ai <nardiniryan5@gmail.com>
2026-06-03 00:58:09 -0300

    Update calc_imc.py
    Removed unused functions and input prompts for IMC calculation.

--- calc_imc.py
+++ calc_imc.py
@@ -6,7 +6,6 @@ def calcular_imc(p, a):
     return imc
 
 #Módulo 2: Classificação de Dados
-#Módulo 3: Especialista em Conteúdo
 #Responsável: Rafael Henrique Trajano
 def classificar(valor_imc):
     if valor_imc <25:
@@ -14,18 +13,3 @@ def classificar(valor_imc):
     else:
         print("Acima do peso 🚫")
         
-def gerar_aviso(status):
-    if imc <25:
-        print("Dica: Continue mantendo hábitos saudáveis, como uma dieta equilibrada e exercícios regulares!")
-    else:
-        print("Dica: Inclua caminhadas na sua rotina e foque em alimentos ricos em fibras. Pequenas mudanças fazem grande diferença!")
-
-#Módulo 4:Engenheiro de Integração
-#Responsável: Ryan Nardini Pereira
-p = float(input("Qual o Seu Peso atual? "))
-a = float(input("Qual sua altura atual? "))
- 
-imc = calcular_imc(p, a)
-print(f"Seu IMC é: {imc:.2f}")
-classificar(imc)
-gerar_aviso(imc) 
